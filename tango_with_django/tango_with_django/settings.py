6ab569a2d688862de66778f319fb98c22456878b
DeathscytheH <alejandro.deathscythe@gmail.com>
2014-08-22 16:15:13 -0500

    Chapter 4
    Pequeño break.

--- tango_with_django/tango_with_django/settings.py
+++ tango_with_django/tango_with_django/settings.py
@@ -1,5 +1,12 @@
 # Django settings for tango_with_django project.
 
+#Variables auxiliares
+import os
+SETTINGS_DIR = os.path.dirname(__file__)
+PROYECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
+PROYECT_PATH = os.path.abspath(PROYECT_PATH)
+TEMPLATE_PATH = os.path.join(PROYECT_PATH, 'templates')
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -111,6 +118,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    TEMPLATE_PATH,
+
 )
 
 INSTALLED_APPS = (
